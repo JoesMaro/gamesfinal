dab5786c8457245d0829cc4a0bc5c253c2647f27
JoesMaro <playanim96@gmail.com>
2024-03-09 08:17:17 -0500

    primera web

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const enviarFormulario = (event) => {
+    event.preventDefault();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const sexo = document.querySelector('input[name="sexo"]:checked');
+    const preferencias = document.getElementById("preferencias").value.trim();
+    const telefono = document.getElementById("telefono").value.trim();
+    const email = document.getElementById("email").value.trim();
+
+    if (!nombre || !apellido || !sexo || !preferencias || !telefono || !emailRegex.test(email)) {
+        alert("Por favor, complete todos los campos correctamente.");
+        return false;
+    }
+
+    alert("El formulario se ha enviado correctamente.");
+    document.getElementById("formulario-contacto").reset();
+
+    return false;
+};
+
+document.getElementById("formulario-contacto").onsubmit = enviarFormulario;
